2b84a9efdec094ee96f461ff2bb6498300d4744f
Matt <matt.snell.00@hotmail.co.uk>
2020-07-21 21:54:52 +0000

    set up basic app.py to run test home_page.html

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "recipe_website"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def home_page():
+    return render_template('home_page.html')
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP"),
+            port=int(os.getenv("PORT")),
+            debug=True)
