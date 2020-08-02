1c4e449174e167c0c4ee3ba8a86cf55e9ee9c3a2
Matt <matt.snell.00@hotmail.co.uk>
2020-08-02 22:14:08 +0000

    add function comments to app.py file

--- app.py
+++ app.py
@@ -18,16 +18,21 @@ app.secret_key = os.getenv('SECRET_KEY')
 @app.route("/")
 @app.route("/get_recipes")
 def home_page():
+    """ displays the home page as well as a list of available recipes"""
     return render_template('home_page.html', recipes=mongo.db.recipes.find())
 
 
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
+    """ displays the create account page/form """
     return render_template('create_account.html')
 
 
 @app.route("/insert_account", methods=["POST"])
 def insert_account():
+    """ (from "create_account" submit) checks if the username is already in the
+database and if the double entered passwords match, if the username is not
+taken and the passwords match, a new user is created in the database """
     user = mongo.db.users.find_one({"user_name": request.form.get(
         'username').lower()})
     if user:
@@ -41,6 +46,7 @@ def insert_account():
                 "email_address": request.form.get('email'),
                 "password": request.form.get('password')
             })
+            flash("account created")
         else:
             flash("Your passwords didn't match")
             return redirect(url_for('create_account'))
@@ -49,11 +55,16 @@ def insert_account():
 
 @app.route("/user_login", methods=["GET", "POST"])
 def user_login():
+    """ displays the login page/form """
     return render_template('user_login.html')
 
 
 @app.route("/login", methods=["POST"])
 def login():
+    """ (from "user_login" submit) checks if the username is in the
+    database and if so, that the passwords match, if the username is present
+    and the passwords match, the user is logged in by way of storing the
+    username in session """
     user = mongo.db.users.find_one({"user_name": request.form.get(
         'username').lower()})
     if user:
